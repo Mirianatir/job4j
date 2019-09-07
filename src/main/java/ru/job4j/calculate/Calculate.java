c3146b01d046b9289af7000fdf31a2a4c0eb3a8e
Mirianatir <sashaklimov65@gmail.com>
2019-09-07 20:32:36 +0300

    3

--- src/main/java/ru/job4j/calculate/Calculate.java
+++ src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j.calculate;
 
 public class Calculate{
-	
+	 /**
+	 * Calculate.
+	 * @author Klimov Alexandr
+	 */
+
+	/**
+	 *
+	 * @param args
+	 */
+
 	public static void main(String[] args){
 		System.out.println("Hello World");
 	}
---
+++ src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Klimov Alexandr
+ * @version $3.0$
+ * @since 0.1
+ */
